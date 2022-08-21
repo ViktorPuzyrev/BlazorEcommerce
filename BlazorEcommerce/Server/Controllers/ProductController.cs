0d27e4324b0b01ace2bf7eedc113e9143eba6c5c
Viktor Puzyrev <v.puzyrev.pktm@gmail.com>
2022-08-21 21:03:08 +0400

    Implement a Service Response with Generics

--- BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -12,10 +12,14 @@ namespace BlazorEcommerce.Server.Controllers
             _context = context;
         }
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProduct()
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProduct()
         {
             var products = await _context.Products.ToListAsync();
-            return Ok(products);
+            var response = new ServiceResponse<List<Product>>()
+            {
+                Data = products
+            };
+            return Ok(response);
         }
     }
 }
