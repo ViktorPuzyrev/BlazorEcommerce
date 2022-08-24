e2020a44d861bcd8e5eb86cf2edd6050a4146492
Viktor Puzyrev <v.puzyrev.pktm@gmail.com>
2022-08-24 20:51:51 +0400

    Add registration page

---
+++ BlazorEcommerce/Shared/UserRegister.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorEcommerce.Shared
+{
+    public class UserRegister
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required, StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; } = string.Empty;
+        [Compare("Password", ErrorMessage = "Пароль не совпадает.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
